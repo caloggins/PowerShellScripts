14e8ba81f8286caa24844cfc1879204be12a9e71
Christopher Loggins <christopher.loggins@gmail.com>
2013-02-20 07:43:59 -0600

    TopShelf script
    A little script which helps w/ TopShelf services.

---
+++ TopShelfHelper.ps1
@@ -0,0 +1,28 @@
+﻿param(
+	[parameter(mandatory=$true)]
+	[validateset("start","stop","uninstall", "install")]
+	$command
+)
+
+$ErrorActionPreference = "Stop"
+
+push-Location
+
+try
+{
+	get-ChildItem -Path $pwd -Recurse |
+		where-Object {$_.name -eq 'topshelfhost.exe'} |
+		select-Object fullname |
+		foreach-object {& $_.fullname $command}
+
+	"INFO: Operation completed successfully."
+}
+catch
+{
+	$Error[0]
+	"ERROR: An error occurred performing the operation."
+}
+finally
+{
+	pop-Location
+}
